4f1375fd7822402af480438d6ae3b67eda3c5366
Martin Pépin <kerl@wkerl.me>
2019-11-22 13:56:38 +0800

    Compare bool generators

--- src/bin/bench.ml
+++ src/bin/bench.ml
@@ -1,6 +1,6 @@
 open Boltzexp
 
-let target = 200_000_000
+let target = 50_000_000
 
 let bench gen =
   let open Benchtools in
@@ -10,36 +10,45 @@ let bench gen =
   res
 
 let benchall l =
-  let l = List.map (fun (name, gen) -> (name, bench gen)) l in
-  let compare (_, (_, t)) (_, (_, t')) = Float.compare t t' in
-  let l = List.sort compare l in
-  let _, (n, t0) = List.hd l in
-  let n = float_of_int n in
-  Format.printf "name:runtime:slowdown:ns/node@.";
-  let print_res name t =
-    Format.printf "%s:%Fs:%.3fx:%.3f@."
-      name t (t0 /. t) ((t /. n) *. 1e9)
+  let l =
+    l
+    |> List.map (fun (s1, s2, gen) -> (s1, s2, bench gen))
+    |> List.map (fun (s1, s2, (n, t)) -> s1, s2, n, (t /. float_of_int n) *. 1e9)
+    |> List.sort (fun (_, _, _, r1) (_, _, _, r2) -> Float.compare r1 r2)
   in
-  List.iter (fun (name, (_, t)) -> print_res name t) l
+  let _, _, _, r0 = List.hd l in
+  Format.printf "generator:bool_sampler:# of nodes:ns/node:slowdown@.";
+  let print_res (s1, s2, n, r) =
+    Format.printf "%s:%s:%d:%.3f:%.3fx@." s1 s2 n r (r /. r0)
+  in
+  List.iter print_res l
+
+let combine rand_bool sampler =
+  let sampler_name, sampler = sampler in
+  let rand_bool_name, rand_bool = rand_bool in
+  sampler_name, rand_bool_name, (fun () -> sampler rand_bool)
 
 let () =
   Rand.init 41329213424289;
   let open Bintree in
 
-  let l = [
+  let samplers = [
     "constant space", ConstantSpace.free_size;
-    "stack-based", StackBased.free_size;
-    "gadt-stack", Gadt.free_size;
-    "generic", Generic.free_size;
+    (* "stack-based", StackBased.free_size; *)
+    (* "gadt-stack", Gadt.free_size; *)
+    (* "generic", Generic.free_size; *)
     "arbogen", Arbogen.free_size;
   ] in
-  let ll =
-    List.fold_right
-      (fun (name, f) acc ->
-        (name, fun () -> f Rand.bool)
-        :: (name ^ "'", fun () -> f Random.bool)
-        :: acc)
-      l
-      []
+  let bool_samplers = [
+    "float", (fun () -> Rand.float 1. < 0.5);
+    "ocaml", Random.bool;
+    "bit-optimal", Rand.bool;
+  ] in
+  let all =
+    List.map
+      (fun sampler ->
+        List.map (fun rand_bool -> combine rand_bool sampler) bool_samplers)
+      samplers
+    |> List.flatten
   in
-  benchall ll
+  benchall all
